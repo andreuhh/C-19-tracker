36768218365b95cc00d29161132bb63d6fefd3de
Andrea Guffi <andreaguffi92@gmail.com>
2021-10-06 22:45:28 +0200

    finished

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,37 @@
+<template>
+  <select
+    @change="onChangeCountry"
+    v-model="selected"
+    class="form-select mt-5 block w-full border p-3 rounded"
+  >
+    <option value="0">Select Country</option>
+    <option v-for="country in countries" :key="country.ID" :value="country.ID">
+      {{ country.Country }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "CountrySelect",
+  props: ["countries"],
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  methods: {
+    onChangeCountry() {
+      //fins selected country
+      const country = this.countries.find((item) => item.ID === this.selected);
+
+      console.log(country);
+
+      this.$emit("get-country", country);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,24 @@
 <template>
   <main v-if="!loading">
     <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
 
-    <h1>min 50</h1>
+    <button
+      v-if="stats.Country"
+      class="
+        bg-green-700
+        text-white
+        rounded
+        p-3
+        mt-10
+        focus:outline-none
+        hover:bg-green-600
+      "
+      @click="clearCountryData"
+    >
+      Clear country
+    </button>
   </main>
 
   <main class="flex flex-col align-center justify-center" v-else>
@@ -13,10 +29,12 @@
 
 <script>
 import DataTitle from "@/components/DataTitle";
+import DataBoxes from "@/components/DataBoxes";
+import CountrySelect from "@/components/CountrySelect";
 
 export default {
   name: "Home",
-  components: { DataTitle },
+  components: { DataTitle, DataBoxes, CountrySelect },
   data() {
     return {
       loading: true,
@@ -32,6 +50,17 @@ export default {
       const res = await fetch("https://api.covid19api.com/summary");
       const data = await res.json();
       return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
     },
   },
   async created() {
